11f7375d9223d7a94e7dc979d55c9d595d4ca238
Twoia-Kotletochka <olenchenko.ilya@ukr.net>
2024-02-13 15:36:22 +0200

    work slide+-

--- main/mac/section_consider/section_consider.js
+++ main/mac/section_consider/section_consider.js
@@ -1,18 +1,29 @@
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const container = document.querySelector('.carousel-container');
-let currentSlide = 0;
+const carouselContainer = document.querySelector(".carousel-container");
+const slides = document.querySelectorAll(".carousel-slide");
+const totalSlides = slides.length;
+let slidePosition = 0;
 
-prevBtn.addEventListener('click', () => {
-  if (currentSlide > 0) {
-    currentSlide--;
-    container.style.transform = `translateX(-${currentSlide * 100}%)`;
-  }
-});
+document.querySelector("#prevBtn").addEventListener("click", moveToPrevSlide);
+document.querySelector("#nextBtn").addEventListener("click", moveToNextSlide);
 
-nextBtn.addEventListener('click', () => {
-  if (currentSlide < container.children.length - 1) {
-    currentSlide++;
-    container.style.transform = `translateX(-${currentSlide * 100}%)`;
-  }
-});
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    moveCarouselTo(slidePosition);
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    moveCarouselTo(slidePosition);
+}
+
+function moveCarouselTo(slidePosition) {
+    carouselContainer.style.transform = 'translateX(-' + slidePosition * 30 + '%)';
+}
