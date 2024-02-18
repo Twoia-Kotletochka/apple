e1f0302f461065e15ea6f87eb43e79b673d6c6b5
Twoia-Kotletochka <olenchenko.ilya@ukr.net>
2024-02-18 14:42:39 +0200

    fin responsive

--- main/mac/section_responsive/section_responsive.js
+++ main/mac/section_responsive/section_responsive.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const accButtons = document.querySelectorAll('.accordion-button');
+    const accButtons = document.querySelectorAll('.accordion-button-container');
     const dynamicImage = document.getElementById('dynamic-image');
     const images = ['/main/mac/section_responsive/section_responsive_img/world_mac_iphone.jpg',
      '/main/mac/section_responsive/section_responsive_img/world_mac_ipad.jpg',
-      '/main/mac/section_responsive/section_responsive_img/world_mac_watch.jpg']; // Update this array with your own image paths
+      '/main/mac/section_responsive/section_responsive_img/world_mac_watch.jpg']; 
 
-    // Open the first accordion by default and set its corresponding image
     accButtons[0].classList.add('active');
     accButtons[0].nextElementSibling.style.display = 'block';
     dynamicImage.src = images[0];
 
     accButtons.forEach(function (btn, index) {
+        const buttonImage = btn.querySelector('#accordion-button-img');
+
+        // Set initial state for images
+        if (btn.classList.contains('active')) {
+            buttonImage.src = '/main/mac/section_responsive/section_responsive_img/arrow.svg';
+        } else {
+            buttonImage.src = '/main/mac/section_responsive/section_responsive_img/arrow_180.svg';
+        }
+
         btn.addEventListener('click', function () {
             const content = this.nextElementSibling;
 
@@ -22,12 +30,18 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (otherBtn !== btn && otherBtn.classList.contains('active')) {
                     otherBtn.classList.remove('active');
                     otherBtn.nextElementSibling.style.display = 'none';
+
+                    let otherButtonImage = otherBtn.querySelector('#accordion-button-img');
+                    otherButtonImage.src = '/main/mac/section_responsive/section_responsive_img/arrow_180.svg';
                 }
             });
 
+            // Set arrow image for active button
+            buttonImage.src = '/main/mac/section_responsive/section_responsive_img/arrow.svg';
+            
             this.classList.add('active');
             content.style.display = 'block';
-            dynamicImage.src = images[index]; // Update the image source when an accordion section is clicked
+            dynamicImage.src = images[index];
         });
     });
 });
